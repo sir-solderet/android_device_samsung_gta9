3f52b73ed01134dd4e827937138d1588e5689f5d
sir-solderet <adityasikarwar573@gmail.com>
2025-09-12 12:06:29 +0530

    build: Switch vendor_dlkm to Soong

--- device.mk
+++ device.mk
@@ -14,8 +14,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
-# Insert vendor_dlkm kernel modules
-$(call inherit-product, $(LOCAL_PATH)/vendor_dlkm.mk)
+# Add the prebuilt vendor_dlkm Soong module
+PRODUCT_PACKAGES += \
+    vendor_dlkm_prebuilt_modules
 
 # Prebuilt DTB and DTBO
 PRODUCT_COPY_FILES += \
